4481aeacf90ce2d3f1c3433123c86b78d5166c2e
GlassPainter <ckwong6599@gmail.com>
2023-05-12 14:01:04 -0400

    Trying to upload api

--- SQLFINAL/src/views/Shop.vue
+++ SQLFINAL/src/views/Shop.vue
@@ -2,29 +2,47 @@
 import { ref, onMounted } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
-const movies = ref([])
+// const movies = ref([])
 
-async function getMovies() {
-  const { data } = await supabase.from('movie').select()
-  movies.value = data
-}
+// async function getMovies() {
+//   const { data } = await supabase.from('movie').select()
+//   movies.value = data
+// }
 
-async function addToCart(name, weekly_rent, genre, poster) {
-  const { upload } = await supabase.from('shopping_cart').insert({
-    name: `${name}`,
-    weekly_rent: `${weekly_rent}`,
-    genre: `${genre}`,
-    poster: `${poster}`
+// async function addToCart(name, price, genre, poster) {
+//   const { upload } = await supabase.from('shopping_cart').insert({
+//     name: `${name}`,
+//     price: `${price}`,
+//     genre: `${genre}`,
+//     poster: `${poster}`
+//   })
+// }
+
+const movieload = ref('')
+async function getDog() {
+  let res = await fetch(`https://amiiboapi.com/api/amiibo/`)
+  let datamovie = await res.json()
+  movieload.value = datamovie
+  const { upload } = await supabase.from('amiibo')
+  movieload.amiibo.forEach(function (element) {
+    upload.insert({
+      name: `${element.name}`,
+      type: `${element.type}`,
+      gameSeries: `${element.gameSeries}`
+    })
   })
 }
 
+//https://amiiboapi.com/api/amiibo/
+
 onMounted(() => {
-  getMovies()
+  getDog()
+  // getMovies()
 })
 </script>
 
 <template class="template">
-  <div id="moviesContainer">
+  <!-- <div id="moviesContainer">
     <div v-for="movie in movies" :key="movies.id" class="individualMovie">
       <div>
         <h2>
@@ -33,12 +51,12 @@ onMounted(() => {
         <img :src="movie.poster" width="200" />
         <p>{{ movie.weekly_rent }}</p>
         <p>{{ movie.genre }}</p>
-        <button @click="addToCart(movie.name, movie.weekly_rent, movie.genre, movie.poster)">
+        <button @click="addToCart(movie.name, movie.price, movie.genre, movie.poster)">
           Add to Cart
         </button>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style>
