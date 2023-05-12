009839458d0c929b6db3cb4feedf04c6e43156ca
GhTmp1A <stanbutter4@gmail.com>
2023-05-12 13:59:47 -0400

    Start of login page

---
+++ SQLFINAL/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const users = ref([])
+
+async function getUsers() {
+  const { data } = await supabase.from('user_stats').select()
+  user.value = data
+  console.log(user)
+}
+</script>
+
+<style lang="scss" scoped></style>
