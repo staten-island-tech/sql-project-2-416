96bab4696d72c6e72b0f256a3cea550291dd2776
GlassPainter <ckwong6599@gmail.com>
2023-05-15 14:01:30 -0400

    Update shop.js

--- SQLFINAL/src/stores/shop.js
+++ SQLFINAL/src/stores/shop.js
@@ -1,22 +1,22 @@
-import { defineStore } from 'pinia'
-import { ref, onMounted } from 'vue'
+// import { defineStore } from 'pinia'
+// import { ref, onMounted } from 'vue'
 
-export const shop = defineStore('shop', {
-  setup() {
-    const amiibos = ref([])
-    onMounted(() => {
-      async function getAmiibos() {
-        const { data } = await supabase.from('amiibo').select()
-        amiibos.value = data
-      }
-      getAmiibos()
-    })
-  },
-  state: () => {
-    return {
-      amiibos
-    }
-  },
-  getters: {},
-  actions: {}
-})
+// export const shop = defineStore('shop', {
+//   setup() {
+//     const amiibos = ref([])
+//     onMounted(() => {
+//       async function getAmiibos() {
+//         const { data } = await supabase.from('amiibo').select()
+//         amiibos.value = data
+//       }
+//       getAmiibos()
+//     })
+//   },
+//   state: () => {
+//     return {
+//       amiibos
+//     }
+//   },
+//   getters: {},
+//   actions: {}
+// })
