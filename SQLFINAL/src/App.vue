a8ddde49f92120fc8dd2c20a6075012d0b0dbc4f
GlassPainter <ckwong6599@gmail.com>
2023-05-16 14:02:40 -0400

    Having trouble with addtocart transition from Vue to Pinia version

--- SQLFINAL/src/App.vue
+++ SQLFINAL/src/App.vue
@@ -1,13 +1,49 @@
-<template>
-  <!-- <Shop></Shop> -->
-</template>
-
 <script setup>
 // import Shop from './views/Shop.vue'
 import { storeToRefs } from 'pinia'
 import { shop } from '@/stores/shop'
-const store = shop()
-const { amiibos } = storeToRefs(store)
+const { amiibos, addToCart } = storeToRefs(shop())
 </script>
 
-<style scoped></style>
+<template class="template">
+  <div id="amiibosContainer">
+    <div v-for="amiibo in amiibos" :key="amiibo.id" class="individualAmiibo">
+      <div>
+        <h2>
+          {{ amiibo.name }}
+        </h2>
+        <img :src="amiibo.image" width="200" />
+        <p>{{ amiibo.gameSeries }}</p>
+        <p>{{ amiibo.character }}</p>
+        <button
+          @click="
+            addToCart(
+              amiibo.amiiboSeries,
+              amiibo.name,
+              amiibo.gameSeries,
+              amiibo.character,
+              amiibo.image
+            )
+          "
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#amiibosContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+.individualAmiibo {
+  border: solid black;
+  width: 27.5%;
+  text-align: center;
+}
+</style>
