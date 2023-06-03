6eef2450be934990243d5fbf62fa07ba66c736ad
Christopher Kwong <ckwong6599@gmail.com>
2023-06-03 12:53:37 -0400

    Sign up works, gonna do login next

--- SQLFINAL/src/views/SignUp.vue
+++ SQLFINAL/src/views/SignUp.vue
@@ -2,29 +2,49 @@
   <HeadingTemplate></HeadingTemplate>
   <form class="sign">
     <label for="email">Username</label>
-    <input id="email" type="text" ref="email" />
+    <input id="email" type="text" v-model="email" />
     <label for="password">Password</label>
-    <input id="password" type="text" ref="password" />
+    <input id="password" type="text" v-model="password" />
     <label for="confirm">Confirm Password</label>
-    <input id="confirm" type="text" ref="confirm" />
-    <button type="submit" @click.prevent="userCreate(this.$refs.email, this.$refs.password)">
-      Create
-    </button>
+    <input id="confirm" type="text" v-model="confirm" />
+    <button v-if="samePassword() && this.password != ''" @click.prevent="createUser">Create</button>
+    <button v-else @click.prevent="createUser" disabled>Create</button>
   </form>
   <!-- <div v-if="userCheck(this.$refs.username.value)">Same</div> -->
 </template>
 
 <script>
 import HeadingTemplate from './HeadingTemplate.vue'
-import { defineComponent } from 'vue'
-import { users } from '@/stores/signup'
+import { defineComponent, ref } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
 export default {
+  data() {
+    return {
+      email: ref(''),
+      password: ref(''),
+      confirm: ref('')
+    }
+  },
   components: {
     HeadingTemplate
   },
-  methods: {}
+  methods: {
+    async createUser(userEmail, userPassword) {
+      const { data, error } = await supabase.auth.signUp({
+        email: this.email,
+        password: this.password
+      })
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(data)
+      }
+    },
+    samePassword() {
+      return this.password == this.confirm
+    }
+  }
 }
 </script>
 
