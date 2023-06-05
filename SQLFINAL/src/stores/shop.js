ea5d50a730f0b74ec50d60c0a3d227860b844f1f
GlassPainter <ckwong6599@gmail.com>
2023-06-05 14:00:49 -0400

    Error on logging in

--- SQLFINAL/src/stores/shop.js
+++ SQLFINAL/src/stores/shop.js
@@ -7,7 +7,7 @@ const userInfo = userInformation()
 export const shop = defineStore('shop', () => {
   async function addToCart(name, image, gameSeries, price, character, amiiboSeries, email) {
     if (userInfo.user.loggedIn == true) {
-      const { upload } = await supabase.from('shopping_cart').insert({
+      const { upload } = await supabase.from('shopping_cart_old').insert({
         name: `${name}`,
         image: `${image}`,
         gameSeries: `${gameSeries}`,
